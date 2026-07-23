23fe6f112a63109f2df33803320505879fd77d85
Amarnath Kolla <amarrama269@gmail.com>
2026-07-23 21:02:47 +0530

    Display welcome message and create sample contact

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
 from model.person import Person
+
+
+print(" Welcome to Address Book Program  ._. 📖 ")
+
+
+person1 = Person(
+    "Amarnath",
+    "Kolla",
+    "chennai",
+    "chennai",
+    "TN",
+    "517501",
+    "9876543210",
+    "amartyuiop@.com"
+)
+
+person1.display()
